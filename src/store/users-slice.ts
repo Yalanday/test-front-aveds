bee11c5a29caf067c0fcba511fa2308295c9d9c9
Yalanday <yalanday256@gmail.com>
2024-09-21 06:08:33 +0300

    first commit

---
+++ src/store/users-slice.ts
@@ -0,0 +1,33 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+export type TypeisAuthenticate = {
+    isAuthenticated: boolean
+};
+
+const setValueAuth = (): boolean => {
+    if (localStorage.getItem('authStatus') === 'true') {
+        return true
+    }
+    return false
+};
+
+const initialState: TypeisAuthenticate = {
+    isAuthenticated: setValueAuth()
+};
+
+console.log(localStorage.getItem('authStatus'));
+
+
+const isAuthenticateSlice = createSlice({
+    name: 'isAuthenticated',
+    initialState,
+    reducers: {
+        setIsAuthenticated: (state, action: PayloadAction<boolean>) => {
+            state.isAuthenticated = action.payload
+        }
+    }
+});
+
+const isAuthenticateReducer = isAuthenticateSlice.reducer;
+export const {setIsAuthenticated} = isAuthenticateSlice.actions;
+export default isAuthenticateReducer;
